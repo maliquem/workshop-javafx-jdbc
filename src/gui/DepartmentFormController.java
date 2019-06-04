56386d9ac64725d6068bf02308547cc249c13ba8
maliquem <jael182@gmail.com>
2019-06-04 16:18:42 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,62 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Department;
+
+public class DepartmentFormController implements Initializable {
+	
+	private Department entity;
+	
+	@FXML
+	private Label labelErrorName;
+	@FXML
+	private TextField txtId;
+	@FXML
+	private TextField txtName;
+	@FXML
+	private Button btSave;
+	@FXML
+	private Button btCancel;
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
+	
+	@FXML
+	public void onBtSaveAction() {
+		
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+		
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	public void updateFormData() {
+		if (entity == null) {
+			throw new IllegalStateException("Entidade esta vazia");
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(String.valueOf(entity.getName()));
+	}
+
+}
